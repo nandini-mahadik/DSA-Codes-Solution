a86a6c545ab07749258c0bf9164b0ee777e5ff40
Akshaya-20012604 <akshaya.n.m.26@gmail.com>
2026-09-07 00:33:25 +0530

    Added pattern 12 solution

---
+++ p12.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class p12 {
+    public static void pattern12(int n) {
+        int space = 2*n-1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+
+            }
+            for (int j = 1; j <= space ; j++) {
+                System.out.print(" ");
+
+            }
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j);
+
+            }
+            System.out.println();
+            space -= 2;
+        }
+
+    }
+
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        pattern12(n);
+
+    }
+}
