8d51586bd7806941c8808e5ba73976d5e205c52d
Akshaya-20012604 <akshaya.n.m.26@gmail.com>
2026-09-07 00:38:04 +0530

    Added pattern 13 solution

---
+++ p13.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class p13 {
+    public static void pattern13(int n) {
+        int num = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(num + " ");
+                num = num + 1;
+
+            }
+            System.out.println();
+
+        }
+
+    }
+
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        pattern13(n);
+
+    }
+}
