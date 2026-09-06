1ee8c576c944b20b59272cf4e42f567de5c3530c
Akshaya-20012604 <akshaya.n.m.26@gmail.com>
2026-09-06 23:51:15 +0530

    Added pattern 10 solution

---
+++ p10.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class p10 {
+    public static void pattern10(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.err.println();
+        }
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]) {
+        System.err.println("Input: ");
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        pattern10(n);
+    }
+}
