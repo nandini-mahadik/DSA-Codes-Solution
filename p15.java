74cc8db22600c7eedb5f19bdd1484626557a1e0e
Nandini Mahadik <nandinimahadik04@gmail.com>
2026-09-07 23:28:03 +0530

    Added pattern 15 solution

---
+++ p15.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class p15 {
+    public static void pattern15(int n) {
+        for (int i = n; i >= 1; i--) {
+            for (char j = 'A'; j <= 'A' + i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        pattern15(n);
+    }
+}
