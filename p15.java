f6378582124c30b36fbe3371711085407f6036e6
Nandini Mahadik <nandinimahadik04@gmail.com>
2026-09-07 23:34:51 +0530

    made changes in pattern 15

--- p15.java
+++ p15.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class p15 {
     public static void pattern15(int n) {
         for (int i = n; i >= 1; i--) {
-            for (char j = 'A'; j <= 'A' + i; j++) {
+            for (char j = 'A'; j < 'A' + i; j++) {
                 System.out.print(j + " ");
             }
             System.out.println();
